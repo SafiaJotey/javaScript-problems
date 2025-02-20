65c7e0c7235a767951548dc0ce84213a8149bc41
safia <safiaahmed1786@gmail.com>
2025-02-20 21:44:03 +0600

    string with unique charecters

---
+++ set/6.js
@@ -0,0 +1,16 @@
+// Write a function that returns the first repeating element in an array.
+// If no element repeats, return null.
+const arr=[1,2,,2,4,3,4,,5,3,5]
+const repeatingElement=(arr)=>{
+
+const newSet= new Set()
+for (let element of arr){
+    if(newSet.has(element)){
+        return element
+    }
+    newSet.add(element)
+}
+}
+
+
+console.log(repeatingElement(arr))
---
+++ set/7.js
@@ -0,0 +1,11 @@
+// Write a function that takes a string and returns a new string with only unique characters.
+
+
+
+const str = "hello world";
+
+const uniqueString=(str)=>{
+    return [...new Set(str)].join('')
+
+}
+console.log(uniqueString(str))
