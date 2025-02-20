24d8e39ab47b1144e9cad11cd51bd09cb34fd1c8
safia <safiaahmed1786@gmail.com>
2025-02-20 12:39:31 +0600

    check duplicates value with set

---
+++ set/5.js
@@ -0,0 +1,10 @@
+// Write a function that checks if an array has duplicate values.
+const arr = [1, 2,  3, 4, 5];
+const haveDuplicates=(arr)=>{
+    if(arr.length=== new Set(arr).size){
+        return false
+    }
+    return true
+
+}
+console.log(haveDuplicates(arr))
