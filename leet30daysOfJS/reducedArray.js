88ebdb54f9523f566159b76eb0ae52cc71d9215c
Safia <safia@shadhinmusic.com>
2026-04-01 11:22:00 +0600

    array methods without built-in functions

---
+++ leet30daysOfJS/reducedArray.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let accum=init;
+    for(let i=0; i<nums.length; i++){
+      
+      accum= fn(accum,nums[i])
+
+    }
+    return accum
+};
+console.log(reduce([1,2,3,4],function sum(accum, curr) { return accum + curr; },0))
---
+++ leet30daysOfJS/transformationArraywithFn.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    let newArray=[]
+    for(let i=0; i<arr.length;i++){
+    newArray.push(    fn(arr[i],i))
+    }
+    return newArray;
+};
+
+console.log(map([1,3,4,5],function (n){return n+1}))
