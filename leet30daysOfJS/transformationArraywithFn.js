7d55d42881bf08f17a6dde450c304882a22c9865
Safia <safia@shadhinmusic.com>
2026-04-06 10:57:57 +0600

    memoized

--- leet30daysOfJS/transformationArraywithFn.js
+++ leet30daysOfJS/transformationArraywithFn.js
@@ -6,7 +6,7 @@
 var map = function(arr, fn) {
     let newArray=[]
     for(let i=0; i<arr.length;i++){
-    newArray.push(    fn(arr[i],i))
+    newArray.push(fn(arr[i],i))
     }
     return newArray;
 };
