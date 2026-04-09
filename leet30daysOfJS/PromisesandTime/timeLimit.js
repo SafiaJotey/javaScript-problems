165655b5f38e4f2e22bd7f62220031e823b9707b
Safia <safia@shadhinmusic.com>
2026-04-09 18:09:19 +0600

    time limit function initiate

---
+++ leet30daysOfJS/PromisesandTime/timeLimit.js
@@ -0,0 +1,13 @@
+var timeLimit = function(fn, t) {
+    
+    return async function(...args) {
+        return new Promise((resolve, reject)=>{
+         
+        })
+    }
+};
+
+
+ const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ 
