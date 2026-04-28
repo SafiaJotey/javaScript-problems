e9e4c1544e2bd1d79ca97e11e5e131dd294b35f5
Safia <safia@shadhinmusic.com>
2026-04-28 10:38:09 +0600

    set creation

---
+++ concepts/set.js
@@ -0,0 +1,14 @@
+//set creation
+
+//create empty and add elements
+const emptySet= new Set()
+console.log("My Empty set", emptySet)
+emptySet.add('el1')
+console.log("With one element",emptySet)
+
+//create with values
+const set1=new Set(["el1", "el2", "el3"])
+console.log("set with intial value",set1)
+
+
+
